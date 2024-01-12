00772a067f4da377218720a7b0aa5086053deac8
Mayer Andres <andres.20.chaves@gmail.com>
2024-01-12 00:39:10 -0500

    Agregando mapper

--- src/config/bcrypt.ts
+++ src/config/bcrypt.ts
@@ -2,9 +2,13 @@ import { hash, compare } from "bcryptjs";
 
 export class BcryptAdapter {
   static async hash(password: string): Promise<string> {
-    const saltRounds = 10;
-    const hashedPassword = await hash(password, saltRounds);
-    return hashedPassword;
+    try {
+      const saltRounds = 10;
+      const hashedPassword = await hash(password, saltRounds);
+      return hashedPassword;
+    } catch (error) {
+      throw error;
+    }
   }
 
   static async compare(password: string, hashed: string): Promise<boolean> {
--- src/infrastructure/datasources/auth.datasource.impl.ts
+++ src/infrastructure/datasources/auth.datasource.impl.ts
@@ -7,6 +7,7 @@ import {
   UserEntity,
   CustomError,
 } from "../../domain";
+import { UserMapper } from "../mappers/user.mapper";
 
 type HashFunction = (password: string) => Promise<string>;
 type CompareFunction = (password: string, hashed: string) => Promise<boolean>;
@@ -27,13 +28,15 @@ export class AuthDatasourceImpl implements AuthDatasource {
         throw CustomError.badRequest("User could not be created");
       }
 
+      const hashedPassword = await this.hashPassword(password);
+
       const user = await UserModel.create({
         name,
         email,
-        password: this.hashPassword(password),
+        password: hashedPassword,
       });
 
-      return new UserEntity(user.id, name, email, user.password, user.roles);
+      return UserMapper.userEntityFromObject(user);
     } catch (error) {
       if (error instanceof CustomError) {
         throw error;
