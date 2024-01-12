3c71bdc53955616244d33dfa915a1303f5f74a1d
Mayer Andres <andres.20.chaves@gmail.com>
2024-01-11 23:01:35 -0500

    Añadiendo Schema para la base de datos

---
+++ src/data/mongodb/models/user.model.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    img: {
+      type: String,
+    },
+    roles: {
+      type: [String],
+      enum: ["ADMIN_ROLE", "USER_ROLE"],
+      default: ["USER_ROLE"],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const UserModel = model("User", userSchema);
