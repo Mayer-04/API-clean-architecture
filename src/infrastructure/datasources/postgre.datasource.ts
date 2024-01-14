4a02de8619e6f8a2a0c6a516580cb90a558008a9
Mayer Andres <andres.20.chaves@gmail.com>
2024-01-13 22:31:43 -0500

    Creando Datasource con Prisma para PostgreSQL

---
+++ src/infrastructure/datasources/postgre.datasource.ts
@@ -0,0 +1,70 @@
+import { PrismaClient } from "@prisma/client";
+import { BcryptAdapter } from "../../config";
+import {
+  AuthDatasource,
+  LoginUserDto,
+  RegisterUserDto,
+  UserEntity,
+  CustomError,
+} from "../../domain";
+import { UserMapper } from "../mappers/user.mapper";
+
+type HashFunction = (password: string) => Promise<string>;
+type CompareFunction = (password: string, hashed: string) => Promise<boolean>;
+
+const prismaClient = new PrismaClient();
+
+export class PostgreAuthDatasource implements AuthDatasource {
+  constructor(
+    private readonly hashPassword: HashFunction = BcryptAdapter.hashPassword,
+    private readonly comparePassword: CompareFunction = BcryptAdapter.comparePassword
+  ) {}
+
+  async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {
+    const { name, email, password } = registerUserDto;
+
+    try {
+      const userExists = await prismaClient.register.findUnique({
+        where: { email },
+      });
+
+      if (userExists) {
+        throw CustomError.badRequest("User could not be created");
+      }
+
+      const hashedPassword = await this.hashPassword(password);
+
+      const user = await prismaClient.register.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      });
+
+      return UserMapper.userEntityFromObject(user);
+    } catch (error) {
+      if (error instanceof CustomError) {
+        throw error;
+      }
+      throw CustomError.internalServerError("Internal server error");
+    }
+  }
+  async login(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    const { email, password } = loginUserDto;
+
+    try {
+      const user = await prismaClient.login.findUnique({ where: { email } });
+
+      if (!user) throw CustomError.badRequest("User does not exists - email");
+
+      const isMatching = await this.comparePassword(password, user.password);
+      if (!isMatching) throw CustomError.badRequest("Password is not valid");
+
+      return UserMapper.userEntityFromObject(user);
+    } catch (error) {
+      console.log(error);
+      throw CustomError.internalServerError("Credentials are not valid");
+    }
+  }
+}
